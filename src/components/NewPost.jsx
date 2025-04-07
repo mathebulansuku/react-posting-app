f711b8adc1505d832aef13273d03ca3602ab9f88
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-04-07 11:37:06 +0200

    added a handling form submission

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,36 @@
 import classes from "./NewPost.module.css";
+import { useState } from "react";
 
-function NewPost({ onAuthorChange, onBodyChange, onCancel }) {
+function NewPost({ onCancel }) {
+  const [change, setChange] = useState("");
+  const [authorChange, setAuthorChange] = useState("");
+
+  function bodyChangeHandler(event) {
+    setChange(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setAuthorChange(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: change,
+      author: authorChange,
+    };
+    console.log(postData);
+    onCancel();
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange} />
+        <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,34 +1,18 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
-import { useState } from "react";
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [change, setChange] = useState("");
-  const [authorChange, setAuthorChange] = useState("");
-
-  function onBodyChangeHandler(event) {
-    setChange(event.target.value);
-  }
-
-  function onAuthorChangeHandler(event) {
-    setAuthorChange(event.target.value);
-  }
   return (
     <>
       {isPosting ? (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={onBodyChangeHandler}
-            onAuthorChange={onAuthorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       ) : null}
       <ul className={classes.posts}>
-        <Post name={authorChange} post={change} />
-        <Post name={authorChange} post={change} />
+        <Post name="Nsuku" post="This is getting confusing" />
       </ul>
     </>
   );
