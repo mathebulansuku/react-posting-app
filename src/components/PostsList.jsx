73e6cf6206c0672dd6a323cd4ea5dc231edf2de2
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-04-12 17:11:42 +0200

    Data fetching with loader

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,23 +1,9 @@
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function PostsList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
+  const posts = useLoaderData();
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -31,7 +17,7 @@ function PostsList() {
   }
   return (
     <>
-      {!isFetching && posts.length > 0 ? (
+      {posts.length > 0 ? (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
